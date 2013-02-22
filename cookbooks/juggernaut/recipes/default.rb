645f2913e5e5b0b408466b873a8302af730e9dd9
Alexandre Klein <alex@dreamexchange.co.jp>
2013-02-22 12:09:10 +0900

    change path of installation

--- cookbooks/juggernaut/recipes/default.rb
+++ cookbooks/juggernaut/recipes/default.rb
@@ -5,9 +5,11 @@
 # Install and start Juggernaut server
 #
 # IMPORTANT: This has to run AFTER node.js is installed
+# /home/deploy/bin/juggernaut
+# juggernaut --port 8080
 
 if ['app','app_master','solo'].include?(node[:instance_role])
-  install_dir = "/usr/local/bin"
+  install_dir = "/home/deploy/bin"
 
   ey_cloud_report "juggernaut" do
     message "Setting up juggernaut server"
@@ -19,7 +21,9 @@ if ['app','app_master','solo'].include?(node[:instance_role])
   end
 
   execute "start juggernaut daemon" do
-    command "/sbin/start-stop-daemon --start --background --exec #{install_dir}/juggernaut --chuid root:root"
+    #command "/sbin/start-stop-daemon --start --background --exec #{install_dir}/juggernaut --chuid root:root"
+    command "screen -S juggernaut"
+    command "juggernaut --port 8080"
 # not_if { FileTest.exists?("#{install_dir}/juggernaut") }
   end
 
